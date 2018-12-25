4ecfeaeb1353d6a5e5fda6c8ca9f9672580c5993
Fady Nasser <fady_nasser9@hotmail.com>
2018-12-25 05:54:25 +0200

    Add files via upload

---
+++ resources/views/reviews/show.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('content')
+    <a href="/reviews" class =" btn btn-default">Go Back</a>
+    <h1>{{$review->title}}</h1>
+    
+    <div>
+        {!!$review->description!!}
+    </div>
+
+    <hr>
+    
+
+    <small>Written on {{$review->created_at}} by {{$review->name}}</small>
+    
+    <hr>
+
+    @if(!Auth::guest())
+
+        @if (auth()->user()->Type == 3)
+            <a href="/reviews/{{$review->id}}/edit" class="btn btn-default">Edit</a>
+            
+            {!!Form::open(['action' => ['PostsController@destroy',$review->id],'method' => 'Post','class' => 'pull-right'])!!}
+                {{Form::hidden('_method','DELETE')}}
+                {{Form::submit('Delete',['class' => 'btn btn-danger'])}}
+            {!!Form::close()!!}
+        @endif
+    @endif
+    
+@endsection
